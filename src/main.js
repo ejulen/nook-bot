549c290c67e205350201afe2bd3633e11ed7add5
Johannes Ulén <johannes@ulen.dev>
2020-04-09 18:22:20 +0000

    Trying a new mention-detection thingy.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ const Eris = require("eris");
 const { createDodoChannel, closeDodoChannel } = require("./handlers/dodo");
 const {
   registerTurnipPrice,
-  setupTurnipPriceClearer
+  setupTurnipPriceClearer,
 } = require("./handlers/turnips");
 const { changeNickname } = require("./handlers/nickname");
 const { changePersonality } = require("./handlers/personality");
-const { help } = require('./handlers/help');
+const { help } = require("./handlers/help");
 const { BOT_TOKEN } = require("./config");
 
 const MESSAGE_ROUTER = [
@@ -34,15 +34,15 @@ const MESSAGE_ROUTER = [
 
 const bot = new Eris.Client(BOT_TOKEN);
 
-bot.on("messageCreate", async message => {
+bot.on("messageCreate", async (message) => {
   if (message.channel.type !== Eris.Constants.ChannelTypes.GUILD_TEXT) {
     return;
   }
 
-  if (message.mentions.some(user => user.id === bot.user.id)) {
-    const content = message.cleanContent
-      .replace(`@${bot.user.username}`, "")
-      .trim();
+  if (message.mentions.some((user) => user.id === bot.user.id)) {
+    const content = message.content
+      .trim()
+      .replace(new RegExp(`^<@!?${bot.user.id}>\\s*`), "");
 
     for (let handler of MESSAGE_ROUTER) {
       const match = content.match(handler.PATTERN);
@@ -53,7 +53,7 @@ bot.on("messageCreate", async message => {
               message,
               bot,
               channel: message.channel,
-              guild: message.channel.guild
+              guild: message.channel.guild,
             },
             match.groups
           );
